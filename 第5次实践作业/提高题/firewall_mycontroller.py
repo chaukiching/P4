ebeb1774523eda3ca8ec1f19dc9be11b5699d807
chaukiching <90104946+chaukiching@users.noreply.github.com>
2022-05-20 13:06:08 +0800

    Update firewall_mycontroller.py

--- 第5次实践作业/提高题/firewall_mycontroller.py
+++ 第5次实践作业/提高题/firewall_mycontroller.py
@@ -18,7 +18,7 @@ from p4runtime_lib.switch import ShutdownAllSwitchConnections
 
 def writecheck_ports(p4info_helper, ingress_sw,
                      ingress_port, egress_spec, dire):
-    
+
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyIngress.check_ports", # 定义表名
         match_fields={
@@ -27,7 +27,7 @@ def writecheck_ports(p4info_helper, ingress_sw,
             # 若包头对应的字段与参数匹配，则执行这一条表项的对应动作
         }, # 设置匹配域
         action_name="MyIngress.set_direction", # 定义动作名
-        action_params={ 
+        action_params={
             "dir": dire
         })
     # 需要使用 p4info_helper 解析器来将规则转化为 P4Runtime 能够识别的形式
@@ -36,7 +36,7 @@ def writecheck_ports(p4info_helper, ingress_sw,
 
 def writeipv4_lpm(p4info_helper, ingress_sw,
                      dst_eth_addr, dst_ip_addr, switch_port):
-    
+
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyIngress.ipv4_lpm", # 定义表名
         match_fields={
@@ -44,14 +44,14 @@ def writeipv4_lpm(p4info_helper, ingress_sw,
             # 若包头对应的字段与参数匹配，则执行这一条表项的对应动作
         }, # 设置匹配域
         action_name="MyIngress.ipv4_forward", # 定义动作名
-        action_params={ 
+        action_params={
             "dstAddr": dst_eth_addr,
-            "port": switch_port 
+            "port": switch_port
         })
     # 需要使用 p4info_helper 解析器来将规则转化为 P4Runtime 能够识别的形式
     ingress_sw.WriteTableEntry(table_entry) # 调用 WriteTableEntry ，将生成的匹配动作表项加入交换机
     print("Installed rule on %s" % ingress_sw.name)
-    
+
 def main(p4info_file_path, bmv2_file_path):
     # Instantiate a P4Runtime helper from the p4info file
     p4info_helper = p4runtime_lib.helper.P4InfoHelper(p4info_file_path) # 初始化 p4info_helper
@@ -75,12 +75,18 @@ def main(p4info_file_path, bmv2_file_path):
             address='127.0.0.1:50053',
             device_id=2,
             proto_dump_file='logs/s3-p4runtime-requests.txt')
+        s4 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
+            name='s4',
+            address='127.0.0.1:50054',
+            device_id=3,
+            proto_dump_file='logs/s4-p4runtime-requests.txt')
 
         # Send master arbitration update message to establish this controller as
         # master (required by P4Runtime before performing any other write operation)
         s1.MasterArbitrationUpdate()
         s2.MasterArbitrationUpdate()
         s3.MasterArbitrationUpdate()
+        s4.MasterArbitrationUpdate()
 
         # Install the P4 program on the switches
         # 在交换机上安装 P4 程序
@@ -93,6 +99,9 @@ def main(p4info_file_path, bmv2_file_path):
         s3.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
                                        bmv2_json_file_path=bmv2_file_path)
         print("Installed P4 Program using SetForwardingPipelineConfig on s3")
+        s4.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
+                                       bmv2_json_file_path=bmv2_file_path)
+        print("Installed P4 Program using SetForwardingPipelineConfig on s4")
 
         writecheck_ports(p4info_helper, ingress_sw=s1, ingress_port=1, egress_spec=3, dire=0)
         writecheck_ports(p4info_helper, ingress_sw=s1, ingress_port=1, egress_spec=4, dire=0)
@@ -110,7 +119,7 @@ def main(p4info_file_path, bmv2_file_path):
                          dst_eth_addr="08:00:00:00:03:00", dst_ip_addr=["10.0.3.3", 32], switch_port=3)
         writeipv4_lpm(p4info_helper, ingress_sw=s1,
                          dst_eth_addr="08:00:00:00:04:00", dst_ip_addr=["10.0.4.4", 32], switch_port=4)
-    
+
         writeipv4_lpm(p4info_helper, ingress_sw=s2,
                          dst_eth_addr="08:00:00:00:03:00", dst_ip_addr=["10.0.1.1", 32], switch_port=4)
         writeipv4_lpm(p4info_helper, ingress_sw=s2,
@@ -119,7 +128,7 @@ def main(p4info_file_path, bmv2_file_path):
                          dst_eth_addr="08:00:00:00:03:33", dst_ip_addr=["10.0.3.3", 32], switch_port=1)
         writeipv4_lpm(p4info_helper, ingress_sw=s2,
                          dst_eth_addr="08:00:00:00:04:44", dst_ip_addr=["10.0.4.4", 32], switch_port=2)
-        
+
         writeipv4_lpm(p4info_helper, ingress_sw=s3,
                          dst_eth_addr="08:00:00:00:01:00", dst_ip_addr=["10.0.1.1", 32], switch_port=1)
         writeipv4_lpm(p4info_helper, ingress_sw=s3,
@@ -128,19 +137,19 @@ def main(p4info_file_path, bmv2_file_path):
                          dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.3.3", 32], switch_port=2)
         writeipv4_lpm(p4info_helper, ingress_sw=s3,
                          dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.4.4", 32], switch_port=2)
-        
+
         writeipv4_lpm(p4info_helper, ingress_sw=s4,
                          dst_eth_addr="08:00:00:00:01:00", dst_ip_addr=["10.0.1.1", 32], switch_port=2)
         writeipv4_lpm(p4info_helper, ingress_sw=s4,
                          dst_eth_addr="08:00:00:00:01:00", dst_ip_addr=["10.0.2.2", 32], switch_port=2)
         writeipv4_lpm(p4info_helper, ingress_sw=s4,
-                         dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.4.4", 32], switch_port=1)
+                         dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.3.3", 32], switch_port=1)
         writeipv4_lpm(p4info_helper, ingress_sw=s4,
-                         dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.1.1", 32], switch_port=1)
-        
+                         dst_eth_addr="08:00:00:00:02:00", dst_ip_addr=["10.0.4.4", 32], switch_port=1)
+
         while True:
             sleep(2)
-            
+
     except KeyboardInterrupt:
         print(" Shutting down.")
     except grpc.RpcError as e:
